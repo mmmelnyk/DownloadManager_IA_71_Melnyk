30fef91e0dba766179e949deef241b253f82888d
Mykola Melnyk <kolanio757@gmail.com>
2020-04-04 21:23:18 +0300

    add mvvm structure; Views: download, home, addurl, setting; Resources dir

---
+++ DownloadManager/Views/HomeView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DownloadManager.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class HomeView : Window
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
