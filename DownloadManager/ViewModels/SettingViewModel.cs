2ed67c4564c68821e5d028d9ed1bc8386b347f73
Mykola Melnyk <kolanio757@gmail.com>
2020-04-09 02:37:42 +0300

    add SetingViewModelLogic

--- DownloadManager/ViewModels/SettingViewModel.cs
+++ DownloadManager/ViewModels/SettingViewModel.cs
@@ -4,11 +4,65 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace DownloadManager.ViewModels
 {
     class SettingViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public void OnPropertyChanged(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public event EventHandler Closing;
+
+        public bool Validate = false;
+
+        private RelayCommand _browsePath;
+        private RelayCommand _savePath;
+
+        private string _defaultPath;
+
+        public string DefaultPath
+        {
+            get => _defaultPath;
+            set
+            {
+                _defaultPath = value;
+                OnPropertyChanged(nameof(DefaultPath));
+            }
+        }
+
+        public RelayCommand BrowsePath
+        {
+            get
+            {
+                return _browsePath ??
+                       (_browsePath = new RelayCommand(o =>
+                       {
+                           FolderBrowserDialog fbd = new FolderBrowserDialog();
+                           if (fbd.ShowDialog() == DialogResult.OK)
+                           {
+                               DefaultPath = fbd.SelectedPath;
+                           }
+                       }));
+            }
+        }
+
+        public RelayCommand SavePath
+        {
+            get
+            {
+                return _savePath ??
+                       (_savePath = new RelayCommand(o =>
+                       {
+                           //smth
+                           Closing?.Invoke(this, EventArgs.Empty);
+                       }));
+            }
+        }
     }
 }
